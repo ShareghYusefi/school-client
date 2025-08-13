b9b3f3ad0e791849bb78aaa09dc8cdafe701ccbf
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-13 16:36:13 -0600

    creating components

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Custom } from './components/custom/custom';
+import { Custom2 } from './components/custom2/custom2';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Custom,
+    Custom2
   ],
   imports: [
     BrowserModule,
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('school-client');
+  protected title = 'School Application';
 }
