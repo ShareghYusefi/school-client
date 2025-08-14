224bf7bb400b433eaf344594565cd49d3239c1d6
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-13 19:57:04 -0600

    class binding & ngClass directive

--- src/app/components/course/course.ts
+++ src/app/components/course/course.ts
@@ -10,6 +10,7 @@ import { Courses } from '../../services/courses';
 export class Course {
   title: string = 'List of Courses';
   courses: string[] = [];
+  booleanVariable = false;
 
   // Dependency Injection
   // When this component is created, angular finds and injects an instance of the Courses Service class and stores it into serviceVariable within constructor function.
